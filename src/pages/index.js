fe6535bbb53e859d6211efcb1e728cf2a7703611
ulngollm <ul@noknok.ru>
2021-05-21 16:57:16 +0300

    add glide slider to index block

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,31 @@
-const videoWrapper = document.querySelector('.video-wrapper');
-const video = document.querySelector('video');
+const videoWrapper = document.querySelector(".video-wrapper");
+const video = document.querySelector("video");
 video.onclick = function () {
-    if (!document.fullscreen) {
-        videoWrapper.requestFullscreen();
-        video.controls = true;
-    }
-}
-document.querySelector('.js-video-close').addEventListener('click', function () {
+  if (!document.fullscreen) {
+    videoWrapper.requestFullscreen();
+    video.controls = true;
+  }
+};
+document
+  .querySelector(".js-video-close")
+  .addEventListener("click", function () {
     document.exitFullscreen();
-})
-document.addEventListener('fullscreenchange', function (e) {
-    if (!document.fullscreen)
-        video.controls = false;
-})
+  });
+document.addEventListener("fullscreenchange", function (e) {
+  if (!document.fullscreen) video.controls = false;
+});
+
+// index slider
+const indexSliderContainer = document.querySelector(".index-slider");
+const sizes = indexSliderContainer.getBoundingClientRect();
+const triggerCenter = sizes.x + sizes.width / 2;
+function nextSlide(e, center, slider) {
+  e.clientX >= center ? slider.go(">") : slider.go("<");
+}
+indexSliderContainer.addEventListener("click", (e) =>
+  nextSlide(e, triggerCenter, indexSlider)
+);
+const indexSlider = new Glide(indexSliderContainer, {
+  rewind: false,
+  autoplay: 20000,
+}).mount();

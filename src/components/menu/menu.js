d5299b960efc00c79e69c679336b9903255e56f4
ulngollm <ulngollm@gmail.com>
2021-05-26 02:05:06 +0300

    update menu and menu animation

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,5 +1,23 @@
 const menuToggle = document.querySelector('.js-menu-toggle');
 const mobileMenu = document.querySelector('.js-menu');
 menuToggle.addEventListener('click', function(){
-    mobileMenu.classList.toggle('menu-mobile_closed');
+    if(mobileMenu.classList.contains('menu-mobile_closed')) openMenu();
+    else closeMenu();
 })
+function openMenu(){
+    mobileMenu.classList.remove('menu-mobile_closed');
+    // document.body.addEventListener('click',closeMenu);
+}
+function closeMenu(e){
+    mobileMenu.style.animationPlayState = 'running';
+    // console.log(e.target);
+    // if(!mobileMenu.contains(e.target)) {
+    //     document.body.removeEventListener('click', closeMenu)
+    // }
+}
+mobileMenu.onanimationend = function(e){
+    e.target.classList.add('menu-mobile_closed');
+}
+mobileMenu.onanimationiteration = function(e){
+    e.target.style.animationPlayState = 'paused';
+}

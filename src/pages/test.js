18c506cd5297ee3f2d01fbfa28ccb828d3a8d629
ulngollm <ul@noknok.ru>
2021-05-26 20:24:21 +0300

    update contact form, add swipe map resize on mobile, complete submit behaviour

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,13 +1,60 @@
-let target = document.querySelector('.scrollMouse');
-target.addEventListener('wheel', function(e){
-  const toLeft  = e.deltaY > 0 && target.scrollLeft < 0;
-  const toRight = e.deltaY < 0 && target.scrollLeft < target.scrollWidth - target.clientWidth;
-  // console.log(e.deltaY);
-  console.log(target.scrollLeft);
+document.querySelector(".js-form-expand").addEventListener("click", formExpand);
+function formFold() {
+  document.querySelector(".js-form-wrapper").classList.add("form-wrapper_fold");
+}
+function formExpand() {
+  document.querySelector(".js-form-wrapper").classList.remove("form-wrapper_fold");
+}
 
-  if (toLeft || toRight) {
-    event.preventDefault();
-    let offset = e.deltaY > 0? 1: -1;
-     e.currentTarget.scrollBy(offset*40, 0);
+const swipeSensitivity = 20;
+const map = document.getElementById("map");
+var touchStart = null;
+var touchPosition = null;
+map.addEventListener("touchstart", touchStartHandle); //
+map.addEventListener("touchmove", touchMoveHandle); //
+map.addEventListener("touchend", touchEndHandle);
+map.addEventListener("touchcancel", touchEndHandle);
+
+function touchStartHandle(e) {
+  fullpage_api.setAllowScrolling(false);
+  touchStartHandle = {
+    x: e.changedTouches[0].clientX,
+    y: e.changedTouches[0].clientY,
+  };
+  touchPosition = { x: touchStartHandle.x, y: touchStartHandle.y };
+}
+function touchMoveHandle(e) {
+  e.stopPropagation();
+  touchPosition = {
+    x: e.changedTouches[0].clientX,
+    y: e.changedTouches[0].clientY,
+  };
+}
+function touchEndHandle(e) {
+  fullpage_api.setAllowScrolling(true);
+  isSwipeDown()? formFold(): formExpand();
+  touchStartHandle = null;
+  touchPosition = null;
+}
+function isSwipeDown() {
+  var d = {
+      x: touchStartHandle.x - touchPosition.x,
+      y: touchStartHandle.y - touchPosition.y,
+    };
+
+  if (Math.abs(d.y) > swipeSensitivity) {
+    if (d.y < 0) {
+      return true;
+    }
+    else return false;
   }
+}
+
+function formSubmit(){
+  setTimeout(()=>document.querySelector('.js-form-wrapper').classList.add('form-wrapper_submitted'), 1000);
+ 
+}
+document.querySelector('.js-form-wrapper form').addEventListener('submit', function(e){
+  e.preventDefault();
+  formSubmit();
 })

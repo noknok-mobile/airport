af29aa715260dd86c2b0bb364e6b0e8051331fdf
ulngollm <ulngollm@gmail.com>
2021-05-26 12:30:16 +0300

    complete mobile menu

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,30 +1,12 @@
 const menuToggle = document.querySelector('.js-menu-toggle');
 const menuContainer = document.querySelector('.menu-wrapper');
 const mobileMenu = document.querySelector('.menu-mobile');
-menuToggle.addEventListener('click', function(){
-    menuContainer.classList.contains('js-menu_open')? closeMenu(): openMenu();
+menuToggle.addEventListener('click', toggleMenu);
+menuContainer.addEventListener('click', function(e){
+    if(!mobileMenu.contains(e.target))
+        toggleMenu();
 })
-function openMenu(){
-    mobileMenu.style.animationPlayState = 'running';
-    menuContainer.classList.add('js-menu_open');
-
-    // document.body.addEventListener('click',closeMenu);
-}
-function closeMenu(){
-    console.log('clode');
-    mobileMenu.style.animationPlayState = 'running';
-    // console.log(e.target);
-    // if(!mobileMenu.contains(e.target)) {
-    //     document.body.removeEventListener('click', closeMenu)
-    // }
-}
-mobileMenu.onanimationend = function(e){
-    menuContainer.classList.remove('js-menu_open');
-    e.target.style.animationPlayState = 'paused';
-    console.log('end');
-}
-mobileMenu.onanimationiteration = function(e){
-    e.target.style.animationPlayState = 'paused';
-    console.log('iteration');
+function toggleMenu(e){
+    menuContainer.classList.toggle('js-menu_open');
 
 }

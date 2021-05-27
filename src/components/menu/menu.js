19ac50014183694bf9fe52b4547a9d7734f72c65
ulngollm <ul@noknok.ru>
2021-05-27 20:18:07 +0300

    some updates

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,14 +1,14 @@
-if(window.innerWidth < 960){
-    const menuToggle = document.querySelector('.js-menu-toggle');
-    const menuContainer = document.querySelector('.menu-wrapper');
-    const mobileMenu = document.querySelector('.menu-mobile');
-    menuToggle.addEventListener('click', toggleMenu);
-    menuContainer.addEventListener('click', function(e){
-        if(!mobileMenu.contains(e.target) || e.target.classList.contains('menu-mobile__item'))
-            toggleMenu();
-    })
-    function toggleMenu(e){
-        menuContainer.classList.toggle('js-menu_open');
-    }
+const menuToggle = document.querySelector(".js-menu-toggle");
+const menuContainer = document.querySelector(".menu-wrapper");
+const mobileMenu = document.querySelector(".menu-mobile");
+menuToggle.addEventListener("click", toggleMenu);
+menuContainer.addEventListener("click", function (e) {
+  if (
+    !mobileMenu.contains(e.target) ||
+    e.target.classList.contains("menu-mobile__item")
+  )
+    toggleMenu();
+});
+function toggleMenu(e) {
+  menuContainer.classList.toggle("js-menu_open");
 }
-

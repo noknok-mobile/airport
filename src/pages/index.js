220cf658fcff41abb707f76ee2a418fecef52bf5
ulngollm <ul@noknok.ru>
2021-05-28 17:32:24 +0300

    exclude desktop gallery from scroll avoiding

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,12 +17,13 @@ const indexSlider = new Glide(indexSliderContainer, {
 }).mount();
 
 // fullpage
-const myFullpage = new fullpage('#fullpage', {
+let scrollElements = window.innerWidth > 960? ".js-news-list" :'.js-news-list,.gallery__images';
+new fullpage('#fullpage', {
   anchors: ['main', 'about', 'timer', 'indexes', 'gallery','news','contacts'],
   menu: '#menu',
   sectionSelector:'.screen',
   // autoScrolling: true,
-  normalScrollElements:'.js-news-list',
+  normalScrollElements: scrollElements,
   scrollHorizontally: false,
   loopBottom: true,
   loopTop: true

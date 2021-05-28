feb77b680019a02bdb9733714c0359613a3f0845
ulngollm <ul@noknok.ru>
2021-05-28 15:27:09 +0300

    complete gallery on mobile, some refactoring gallery script

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,39 +16,6 @@ const indexSlider = new Glide(indexSliderContainer, {
   autoplay: 20000,
 }).mount();
 
-//gallery
-function showActiveItem(id, className) {
-  const activeClass = `${className}_current`;
-  let lastCurrentElem = document.querySelector(`.${activeClass}`) ?? null;
-  if (lastCurrentElem) lastCurrentElem.classList.remove(activeClass);
-
-  document.querySelector(`[data-link=${id}]`).classList.add(activeClass);
-}
-const imageThumbs = document.querySelectorAll(".js-image-thumb");
-for (let thumb of imageThumbs) {
-  thumb.addEventListener("click", showImage);
-}
-function showImage(e) {
-  const galleryItemName = "gallery__image";
-  let targetId = e.currentTarget.getAttribute("href").replace(/#/, "");
-
-  if (document.querySelector(`[data-link=${targetId}]`))
-    showActiveItem(targetId, galleryItemName);
-}
-
-const videoThumbs = document.querySelectorAll(".js-video-thumb");
-for (let thumb of videoThumbs) {
-  thumb.addEventListener("click", openVideo);
-}
-function openVideo(e){
-    let targetId = e.currentTarget.getAttribute("href").replace(/#/, "");
-    const videoWrapper = document.querySelector(`.gallery__video[data-link=${targetId}] `);
-    console.log(videoWrapper);
-    videoWrapper.classList.add('gallery__video_open');
-    videoWrapper.requestFullscreen();
-
-}
-
 // fullpage
 const myFullpage = new fullpage('#fullpage', {
   anchors: ['main', 'about', 'timer', 'indexes', 'gallery','news','contacts'],

b0ef55c361bd133a4869b12c6be61fec69d9122c
ulngollm <ulngollm@gmail.com>
2021-06-11 22:30:48 +0300

    fix video playing

--- src/components/video/video.js
+++ src/components/video/video.js
@@ -4,18 +4,20 @@ screenfull.on("change", function (e) {
   const video = videoWrapper.querySelector("video");
   if (video) {
     video.controls = screenfull.isFullscreen;
-    video.paused = !screenfull.isFullscreen;
+    if(!video.autoplay) video.paused = !screenfull.isFullscreen;
   }
   fullpage_api.setAllowScrolling(!screenfull.isFullscreen);
 });
 
 function showFullscreenVideo(videoWrapper) {
   const video = videoWrapper.querySelector("video");
+  console.log(video);
   if (screenfull.isEnabled && !screenfull.isFullscreen) {
     screenfull.request(videoWrapper);
+    if(video.autoplay) video.currentTime = 0;
   }
-  if(video.autoplay) video.load();
-  else {
+
+  if(video.paused){
     video.play();
   }
 }

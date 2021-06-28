1c6843ec70e4e7a6a75950162f33ef0082416b8c
ulngollm <ulngollm@gmail.com>
2021-06-29 00:26:57 +0300

    update form validation accorging to page language

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -1,8 +1,10 @@
 // contacts
 document.querySelector(".js-form-expand").addEventListener("click", formExpand);
+
 function formFold() {
   document.querySelector(".js-form-wrapper").classList.add("form-wrapper_fold");
 }
+
 function formExpand() {
   document
     .querySelector(".js-form-wrapper")
@@ -24,9 +26,13 @@ function touchStartHandle(e) {
     x: e.changedTouches[0].clientX,
     y: e.changedTouches[0].clientY,
   };
-  touchPosition = { x: touchStartHandle.x, y: touchStartHandle.y };
+  touchPosition = {
+    x: touchStartHandle.x,
+    y: touchStartHandle.y
+  };
   formFold();
 }
+
 function touchMoveHandle(e) {
   e.stopPropagation();
   touchPosition = {
@@ -34,12 +40,14 @@ function touchMoveHandle(e) {
     y: e.changedTouches[0].clientY,
   };
 }
+
 function touchEndHandle(e) {
   fullpage_api.setAllowScrolling(true);
   if (isSwipeDown()) formFold();
   touchStartHandle = null;
   touchPosition = null;
 }
+
 function isSwipeDown() {
   var d = {
     x: touchStartHandle.x - touchPosition.x,
@@ -53,64 +61,94 @@ function isSwipeDown() {
   }
 }
 
-Inputmask({ mask: "+7 (999) 999-99-99" }).mask('.input[name="phone"]');
+Inputmask({
+  mask: "+7 (999) 999-99-99"
+}).mask('.input[name="phone"]');
 
-const rules = [
-  {
-    name: "name",
-    rules: "required|!callback_word",
-    display: "Имя",
-  },
-  {
-    name: "org",
-    rules: "alpha_numeric|!callback_word",
-    display: "Организация",
-  },
-  {
-    name: "phone",
-    rules: "required|!callback_phone",
-    display: "Телефон",
-  },
-  {
-    name: "email",
-    rules: "valid_email|required",
-    display: "E-mail",
-  },
-  
-];
-const form = document.forms.feedback;
-form.setAttribute('novalidate', '');
-const formMessage = document.querySelector(".form__message");
-form.addEventListener("input", function () {
-  hideErrorMessage(formMessage);
-});
-const validator = new FormValidator("feedback", rules, (errors, event) => {
-  event.preventDefault();
-  errors.forEach((elem) => showErrorMessage(elem.message, formMessage));
-  if(!errors.length) formSubmit(event);
-  console.log(errors);
-});
-validator.setMessage("required", "Поле %s обязательно для заполнения");
-validator.setMessage("valid_email", "Введите корректный %s");
+//form validation
+{
+  const form = document.forms.feedback;
+  form.setAttribute('novalidate', '');
+  const formMessage = document.querySelector(".form__message");
+  form.addEventListener("input", function () {
+    hideErrorMessage(formMessage);
+  });
 
-validator.registerCallback('word', function(value) {
-  const regexp = /^[а-яёa-z\s]*$/gi;
-  return regexp.test(value);
-}).setMessage("word", "Поле %s может содержать только буквы");
+  function showErrorMessage(msg, formMessage) {
+    formMessage.classList.add("form__message_active");
+    formMessage.innerText = msg;
+  }
 
-validator.registerCallback('phone', function(value){
-  const regexp = /[^_]$/gi;
-  return regexp.test(value);
-}).setMessage('phone', "Некорректный номер телефона");
+  function hideErrorMessage(formMessage) {
+    formMessage.classList.remove("form__message_active");
+  }
 
-function showErrorMessage(msg, formMessage) {
-  formMessage.classList.add("form__message_active");
-  formMessage.innerText = msg;
+  const rules = (document.documentElement.lang === 'ru') ? [{
+      name: "name",
+      rules: "required|!callback_cyrillic",
+      display: "Имя",
+    },
+    {
+      name: "org",
+      rules: "!callback_cyrillic",
+      display: "Организация",
+    },
+    {
+      name: "phone",
+      rules: "required|!callback_phone",
+      display: "Телефон",
+    },
+    {
+      name: "email",
+      rules: "valid_email|required",
+      display: "E-mail",
+    },
+  ] : [{
+      name: "name",
+      rules: "required",
+      display: "Name",
+    },
+    {
+      name: "org",
+      rules: "alpha_numeric",
+      display: "Organization",
+    },
+    {
+      name: "phone",
+      rules: "required|!callback_phone",
+      display: "Phone",
+    },
+    {
+      name: "email",
+      rules: "valid_email|required",
+      display: "E-mail",
+    },
+  ];
+  const validator = new FormValidator("feedback", rules, (errors, event) => {
+    event.preventDefault();
+    errors.forEach((elem) => showErrorMessage(elem.message, formMessage));
+    if (!errors.length) formSubmit(event);
+    console.log(errors);
+  });
+
+  validator.registerCallback('cyrillic', function (value) {
+    const regexp = /^[а-яёa-z\s]*$/gi;
+    return regexp.test(value);
+  });
+  validator.registerCallback('phone', function (value) {
+    const regexp = /[^_]$/gi;
+    return regexp.test(value);
+  });
+
+  if (document.documentElement.lang === 'ru') {
+    validator.setMessage("required", "Поле %s обязательно для заполнения");
+    validator.setMessage("valid_email", "Введите корректный %s");
+    validator.setMessage("cyrillic", "Поле %s может содержать только буквы");
+    validator.setMessage('phone', "Некорректный номер телефона");
+  } else
+    validator.setMessage('phone', "Phone is incorrect");
+
 }
-function hideErrorMessage(formMessage) {
-  formMessage.classList.remove("form__message_active");
-}
-
 async function formSubmit(e) {
   e.preventDefault();
   const form = e.target;
@@ -123,6 +161,6 @@ async function formSubmit(e) {
     console.log(result);
     // document.querySelector(".js-form-wrapper").classList.add("form-wrapper_submitted"); //включить на рабочем и удалить ниже
   }
-    // на готовом вырезать
+  // на готовом вырезать
   document.querySelector(".js-form-wrapper").classList.add("form-wrapper_submitted");
 }
